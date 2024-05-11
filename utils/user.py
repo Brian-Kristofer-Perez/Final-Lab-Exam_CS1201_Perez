809314e6e795d8d9ed8556d884cd41e2a8433b71
Brian Perez <briankristoferperez28@gmail.com>
2024-05-11 16:51:04 +0800

    Somewhat workable, I'll take it any day (working UI and import from user_manager.py

---
+++ utils/user.py
@@ -0,0 +1,35 @@
+class User:
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+        self.scores = Scores()
+
+
+
+
+
+class Scores:
+
+    def __init__(self):
+        self.total_score = 0
+        self.stages_won = 0
+
+        self.best_of_3 = BestOf3()
+
+
+class BestOf3:
+
+    def __init__(self):
+        self.wins = 0
+        self.score = 0
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ utils/user_manager.py
@@ -0,0 +1,68 @@
+import os
+import utils.user as user
+
+
+class UserManager:
+
+	def __init__(self):
+
+		# entire block is for initializing files
+
+		if not os.path.exists("utils/data"):  #if no data folder, make files & folder
+			os.makedirs("utils/data")
+			file = open("utils/data/users.txt", "w")
+
+		with open("utils/data/users.txt") as file:
+			if not file.read():
+				pass  # if empty, do not read accounts
+
+			else:  # else read
+				self.load_users()
+
+		self.content = []  #this list will contain player objects
+		self.info = []  #temporary storage for processing
+
+
+
+
+	def load_users(self):
+
+		with open("data/users.txt") as file:
+			name_and_pass = str.split(file.read().strip(), "\n")  # returns a list
+
+			for i in name_and_pass:
+				self.info.append(str.split(i, ": "))
+
+			for name, password in self.info:
+				self.content.append(user.User(name, password))
+
+
+	def save_users(self):
+		pass
+
+	def validate_username(self, username):
+
+		if len(username) < 4:
+			return 1
+
+	def validate_password(self, password):
+		pass
+
+	def register(self):
+		pass
+
+	def login(self):
+		pass
+
+
+
+
+usermanager = UserManager()
+
+
+def main():
+	usermanager.load_users()
+
+
+if __name__ == "__main__":
+	main()
