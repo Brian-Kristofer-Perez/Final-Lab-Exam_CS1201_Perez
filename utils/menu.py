fda835051aaee75bdc0cb47fe2ae895be814ff1b
Brian Perez <briankristoferperez28@gmail.com>
2024-05-11 17:30:21 +0800

    commit before testing (moved UI to inner directory for convenience)

---
+++ utils/menu.py
@@ -0,0 +1,64 @@
+
+import user_manager
+class Menu:
+    def main_menu(self):
+        while True:
+            try:
+                print("Welcome to Dice Roll Game!")
+                print("1. Register")
+                print("2. Log in")
+                print("3. Exit")
+
+                choice = int(input("Enter your choice: "))
+
+                if choice == 1:
+                    user_manager.usermanager.register()
+                    break
+                elif choice == 2:
+                    # accounts.log_in()
+                    break
+                elif choice == 3:
+                    print("Thank you for playing")
+                    exit(0)
+                else:
+                    print("Enter a valid input\n")
+
+            except ValueError:
+                print("Invalid choice\n")
+
+
+    def login_menu(self, username):
+        while True:
+            try:
+
+                print(f"Welcome, {username}!")
+
+                print("Log in Menu")
+                print("1. Start Game")
+                print("2. View Leaderboard")
+                print("3. Log out")
+
+                choice = int(input("Enter choice: "))
+
+                if choice == 1:
+
+                    # game.initialize(username)
+                    # game.game.match()
+
+                    break
+                elif choice == 2:
+                    break
+                    pass
+                elif choice == 3:
+                    # main_menu()
+                    break
+                else:
+                    print("Enter a Valid Input\n")
+            except ValueError:
+                print("Invalid choice\n")
+
+menu = Menu()
+
+if __name__ == "__main__":
+
+    menu.main_menu()
--- utils/user_manager.py
+++ utils/user_manager.py
@@ -1,5 +1,6 @@
 import os
 import utils.user as user
+import menu
 
 
 class UserManager:
@@ -30,7 +31,7 @@ class UserManager:
 	def load_users(self):
 
 		with open("utils/data/users.txt") as file:
-			name_and_pass = str.split(file.read().strip(), "\n")  # returns a list
+			name_and_pass = str.split(file.read().strip(), "\n")  # returns a list, excluding unncecessary newlines and spaces
 
 			for i in name_and_pass:
 				self.info.append(str.split(i, ", "))
@@ -39,19 +40,45 @@ class UserManager:
 				self.content.append(user.User(name, password))
 
 
-	def save_users(self):
-		pass
+	def save_users(self, username, password):
 
-	def validate_username(self, username):
+		with open("utils/data/users.txt", "a") as file:
+			file.write(f"{username}, {password}\n")
+			file.close()
+			usermanager.load_users()
 
-		if len(username) < 4:
-			return 1
-
-	def validate_password(self, password):
-		pass
 
 	def register(self):
-		pass
+		while True:
+			username = input("Input your username (at least 4 characters), or leave blank to return: ")
+
+			if not username:
+				menu.menu.main_menu()
+				break
+
+			if len(username) < 4:
+				print("Please use a name with 4 or more characters.")
+				continue
+
+			if username in map(lambda x: x.name, usermanager.content):
+				print("Username already exists, please try again.")
+				continue
+
+			password = input("Input your password (at least 8 characters), or leave blank to return: ")
+
+			if not password:
+				menu.menu.main_menu()
+				break
+
+			if len(password) < 8:
+				print("Please use a password with 8 or more characters.")
+				continue
+
+
+			usermanager.save_users(username, password)
+			print("Registration successful.\n")
+
+			menu.menu.main_menu()
 
 	def login(self):
 		pass
