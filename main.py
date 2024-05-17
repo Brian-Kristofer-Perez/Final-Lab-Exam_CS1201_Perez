ad6f2f5ec21f406e4e2c7012daa8d941d8e9e3f4
Brian <23-01884@g.batstate-u.edu.ph>
2024-05-17 08:38:17 +0800

    ready!

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import utils.menu as menu
 
 menu.menu.main_menu()
+
+
+"""
+
+Directory Structure:
+
+Project Folder
+    -main.py
+    -utils
+        -menu.py
+        -dice_game.py
+        -user_manager.py
+        -user.py
+        -data
+            -rankings.txt
+            -users.txt
+
+
+
+
+
+
+
+"""
